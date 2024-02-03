f4f504d4184ce70b3d7af55663ee3e538b9c69f7
Denis <benato.denis96@gmail.com>
2024-02-03 17:19:13 +0100

    fixed build script

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ set -e
 
 rm .config
 cp config-base .config
-./scripts/kconfig/merge_config.sh -m .config ../config-neptune
+./scripts/kconfig/merge_config.sh -m .config ./config-neptune
 
 LLVM=1 make -j$(nproc)
