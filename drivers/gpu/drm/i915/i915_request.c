e24cbcc75157519460ae5b0461d456d9f2ab6384
Denis <benato.denis96@gmail.com>
2024-03-04 23:25:30 +0100

    Revert "drm/i915: Drop the irqs_disabled() check"
    This reverts commit d763e4000ab09012226d7daefb0a9c6a69d6c9a4.

--- drivers/gpu/drm/i915/i915_request.c
+++ drivers/gpu/drm/i915/i915_request.c
@@ -609,6 +609,7 @@ bool __i915_request_submit(struct i915_request *request)
 
 	RQ_TRACE(request, "\n");
 
+	GEM_BUG_ON(!irqs_disabled());
 	lockdep_assert_held(&engine->sched_engine->lock);
 
 	/*
@@ -717,6 +718,7 @@ void __i915_request_unsubmit(struct i915_request *request)
 	 */
 	RQ_TRACE(request, "\n");
 
+	GEM_BUG_ON(!irqs_disabled());
 	lockdep_assert_held(&engine->sched_engine->lock);
 
 	/*
