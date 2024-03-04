e6ccd0a483d1dd7c5f49a5f19817038709d74a01
Denis <benato.denis96@gmail.com>
2024-03-04 23:25:38 +0100

    Revert "printk: Bring back the RT bits."
    This reverts commit 84ab7385938cabcd40aef5bd5a017bd091a20e5d.

--- kernel/watchdog.c
+++ kernel/watchdog.c
@@ -431,8 +431,6 @@ static enum hrtimer_restart watchdog_timer_fn(struct hrtimer *hrtimer)
 		/* Start period for the next softlockup warning. */
 		update_report_ts();
 
-		printk_prefer_direct_enter();
-
 		pr_emerg("BUG: soft lockup - CPU#%d stuck for %us! [%s:%d]\n",
 			smp_processor_id(), duration,
 			current->comm, task_pid_nr(current));
@@ -451,8 +449,6 @@ static enum hrtimer_restart watchdog_timer_fn(struct hrtimer *hrtimer)
 		add_taint(TAINT_SOFTLOCKUP, LOCKDEP_STILL_OK);
 		if (softlockup_panic)
 			panic("softlockup: hung tasks");
-
-		printk_prefer_direct_exit();
 	}
 
 	return HRTIMER_RESTART;
